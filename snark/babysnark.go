3255d42d329641ad431b2afa963fa514852172e9
Max Weng <max.m.weng@gmail.com>
2016-09-21 19:18:40 +0800

    Updated the cgo compile flags

--- snark/babysnark.go
+++ snark/babysnark.go
@@ -1,13 +1,18 @@
 package babysnark
 
+// Compile with the following command
+// CC=g++-6 CXX=g++-6 go build github.com/ethereum/go-ethereum/snark
+
 /*
-#cgo CXXFLAGS: -std=c++11
-#cgo CFLAGS: -I./libsnark/src/ -I/usr/include/c++/4.2.1/ -I/usr/include/c++/4.2.1/tr1
-#cgo LDFLAGS: -lgmp
-#include "./hackishlibsnarkbindings.cpp"
+#cgo CPPFLAGS: -Wfatal-errors -Wno-deprecated-declarations -DCURVE_ALT_BN128
+#cgo CPPFLAGS: -I./libsnark/src/ -I/usr/local/include/c++/6.2.0/ -I/usr/local/include/c++/6.2.0/x86_64-apple-darwin14.5.0/
+#cgo LDFLAGS: -lgmpxx -lgmp -lsnark -lsodium -L./libsnark/
+
+extern bool hackishlibsnarkbindings_verify(unsigned char *vk_bytes, unsigned int vk_size, unsigned char *proof_bytes, unsigned int proof_size, unsigned char *primary_input_bytes, unsigned int primary_input_len);
 */
 import "C"
+import "unsafe"
 
 func babySnarkVerify(vk, proof, primary []byte) {
-	C.hackishlibsnarkbindings_verify(vk, len(vk), proof, len(proof), primary, len(primary))
+	C.hackishlibsnarkbindings_verify((*C.unsignedchar)(unsafe.Pointer(&vk[0])), vk.len(), (*C.unsignedchar)(unsafe.Pointer(&proof[0])), proof.len(), (*C.unsignedchar)(unsafe.Pointer(&vprimary[0])), primary.len())
 }
